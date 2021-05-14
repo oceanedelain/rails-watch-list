fc44b197cd4003ff5aa5f53af856bb58560db3db
Oceane Delain <delain.oceane@gmail.com>
2021-05-14 12:42:31 +0200

    root to lists cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [ :index, :show, :new, :create ] do
-    resources :bookmarks, only: [ :new, :create, ]
+  root to: "lists#index"
+
+  resources :lists, only: [ :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: [:destroy]
 end
